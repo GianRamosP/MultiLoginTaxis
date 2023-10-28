6e1f9558ceea250fd88faec18d8954684c1ccadf
gian2102 <gian21022001@gmail.com>
2023-10-28 09:52:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCliente, btnConductor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCliente = findViewById(R.id.btnCliente);
+        btnConductor = findViewById(R.id.btnConductor);
+
+        btnCliente.setOnClickListener(view -> goToSelectAuth());
+    }
+
+    private void goToSelectAuth() {
+        Intent intent = new Intent(this, SelectOptionAuthActivity.class);
+        startActivity(intent);
+    }
+}
